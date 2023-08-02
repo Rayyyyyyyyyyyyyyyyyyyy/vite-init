62063b8ba962055f1af98ee8518af209fe238159
gordon.shao <gordon.shao@dkdtrillion.com>
2023-08-02 17:20:09 +0800

    fix type issue and set public url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
-import { reactive } from "@vue/reactivity"
-import { onMounted } from "@vue/runtime-core"
+import { reactive } from '@vue/reactivity'
 
 const state = reactive({
-
+  count: 1
 })
-
-
 </script>
 
 <template>
   <div class="container">
-    <p> home </p>
+    <p>home {{ state.count }}</p>
   </div>
-
-
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,64 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-import {resolve} from 'path'
-const outDir = resolve(__dirname, "dist")
+import { resolve } from 'path'
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   define: {
-    'APP_VERSION': JSON.stringify(process.env.npm_package_version),
+    APP_VERSION: JSON.stringify(process.env.npm_package_version)
   },
   build: {
     outDir,
-    emptyOutDir:true,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html')
       }
     }
   },
-  assetsInclude: ["**/*.xlsx"],
+  assetsInclude: ['**/*.xlsx'],
 
   plugins: [
     vue(),
     AutoImport({
       imports: ['vue'],
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     }),
     createSvgIconsPlugin({
       iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[name]',
+      symbolId: 'icon-[name]'
     }),
+    createHtmlPlugin({
+      minify: true,
+      pages: [
+        {
+          entry: '/src/main.ts',
+          filename: 'index.html',
+          template: 'index.html',
+          injectOptions: {
+            data: {
+              title: 'The-Trip',
+              icoUrl: './public/vite.svg'
+            }
+          }
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src/")
+      '@': resolve(__dirname, 'src/')
     }
   }
 })
