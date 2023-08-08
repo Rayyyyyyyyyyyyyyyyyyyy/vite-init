2d70606c26b72dce0ded5853420698678b0065ac
gordon.shao <gordon.shao@dkdtrillion.com>
2023-08-08 16:48:28 +0800

    feat tailwind is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,36 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     colors: {
-      colors: {
-        primary: {
-          DEFAULT: '#409eff',
-          light: '#adcdee',
-          dark: '#3560f0'
-        },
-        secondary: {
-          DEFAULT: '#E9EDF0',
-          light: '#F6F6F6',
-          dark: '#707070'
-        },
-        success: {
-          DEFAULT: '#8FC69A',
-          light: '#52c41a'
-        },
-        hr: '#ebedf0',
-        red: {
-          DEFAULT: '#ff0000'
-        },
-        bg: {
-          DEFAULT: '#F5F3F4',
-          light: '#f0f0f0'
-        },
-        text: {
-          DEFAULT: '#000',
-          light: '#e0e4ef'
-        },
-        warning: {
-          DEFAULT: '#e2ab00',
-          dark: '#FFB96F',
-          light: '#FFF3CC'
-        }
+      primary: {
+        DEFAULT: '#409eff',
+        light: '#adcdee',
+        dark: '#3560f0'
+      },
+      secondary: {
+        DEFAULT: '#E9EDF0',
+        light: '#F6F6F6',
+        dark: '#707070'
+      },
+      success: {
+        DEFAULT: '#8FC69A',
+        light: '#52c41a'
+      },
+      hr: '#ebedf0',
+      red: {
+        DEFAULT: '#ff0000'
+      },
+      bg: {
+        DEFAULT: '#F5F3F4',
+        light: '#f0f0f0'
+      },
+      text: {
+        DEFAULT: '#000',
+        light: '#e0e4ef'
+      },
+      warning: {
+        DEFAULT: '#e2ab00',
+        dark: '#FFB96F',
+        light: '#FFF3CC'
       }
     },
 
